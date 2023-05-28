53d535678b2abf66571a5a6632c140d0a83325f8
Kubuuuss <134881016+Kubuuuss@users.noreply.github.com>
2023-05-28 21:31:03 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,61 @@
+const products = [
+{
+    id: 0,
+    name:"Iphone 14",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    category:"Iphone",
+    price: 5999.99,
+    image: "./src/Iphone2.jpg",
+    sale: true,
+    saleAmount:410,
+},
+
+{
+    id: 1,
+    name:"Samsung A72",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    category:"Samsung",
+    price: 2599.99,
+    image:"./src/Samsung.jfif",
+},
+
+{
+    id: 2,
+    name:"Oppo Reno 8t",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    category:"Oppo",
+    price: 1799.99,
+    image: "./src/Oppo.jpg",
+},
+
+{
+    id: 3,
+    name:"Samsung A10",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    category:"Samsung",
+    price: 499.99,
+    image: "./src/a10.webp",
+},
+
+{
+    id: 3,
+    name:"Motorola G71",
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    category:"Motorola",
+    price: 1199.99,
+    image: "./src/g71.jfif",
+    sale: true,
+    saleAmount:240,
+},
+
+{
+id: 4,
+name: "Oppo Reno 6",
+description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+category: "Oppo",
+price:1499.99,
+image:"./src/Oppo6.jpg",
+sale: true,
+saleAmount:130.99,
+},
+];
